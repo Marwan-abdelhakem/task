eba642ea08016ddd2e08923127749e3925acbb04
Marwan abdelhakem <marwanabdelhakem@gmail.com>
2025-12-23 13:44:43 +0200

    commit

--- Src/Modules/user/user.service.js
+++ Src/Modules/user/user.service.js
@@ -210,7 +210,7 @@ export const applayForJob = async (req, res, next) => {
     }
     emailEvent.emit("confirmEmail", { to: email })
     const applayForJob = await dbService.create({ model: newEmployeeModel, data: [{ name, email, phone, age, gender, qualification, experince, cv, status, job_id, cloud }] })
-    return successResponse({ res, statusCode: 201, message: "User Applay successfully", data: applayForJob })
+    return successResponse({ res, statusCode: 201, message: "User Applay successfully", data: cloud })
 }
 
 export const getAlljobs = async (req, res, next) => {
