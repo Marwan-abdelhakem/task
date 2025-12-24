62f40c6d13894c63ddf4ab44e1cd6a4d28b7ce35
Marwan abdelhakem <marwanabdelhakem@gmail.com>
2025-12-24 14:11:45 +0200

    commit1

--- Src/Utlis/cloude.multer.utlis.js
+++ Src/Utlis/cloude.multer.utlis.js
@@ -3,15 +3,15 @@ import multer from "multer"
 
 export const cloudFileUpload = () => {
     const storage = multer.diskStorage({})
-    const fileFilter = (req, file, cb) => {
-        if (validation.includes(file.mimetype)) {
-            cb(null, true)
-        } else {
-            return cb(new Error("Invalid File Type"), false)
-        }
-    }
+    // const fileFilter = (req, file, cb) => {
+    //     if (validation.includes(file.mimetype)) {
+    //         cb(null, true)
+    //     } else {
+    //         return cb(new Error("Invalid File Type"), false)
+    //     }
+    // }
     return multer({
-        fileFilter,
+        // fileFilter,
         storage
     })
 }
