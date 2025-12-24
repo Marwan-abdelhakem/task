1d552db6b22c8c393c66bca3ef00d8f7241c73eb
Samahsalah25 <samahsalah2555@gmail.com>
2025-12-24 17:54:17 +0200

    edit index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import bootStrap from "./Src/app.controller.js"
 
+import dotenv from "dotenv";
+dotenv.config();
+
 
 const app = express()
 const port = process.env.PORT
